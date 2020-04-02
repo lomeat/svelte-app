d39c9431e1a7242053ac308f3a14d09e23b9ee5d
lomeat <lom3at@gmail.com>
2020-04-02 20:44:03 +0300

    add ui input

--- src/counter.svelte
+++ src/counter.svelte
@@ -1,5 +1,6 @@
 <script>
   import Button from "./button.svelte";
+  import Input from "./input.svelte";
 
   export let count = 1;
   export let color = {};
@@ -34,44 +35,15 @@
 
 <wrapper style="--text:{color.text}">
   <Button {color} onmousedown={dec} onmouseup={mouseUp}>-</Button>
-  <input bind:value={count} type="number" />
+  <Input {color} bind:value={count} type="number" />
   <Button {color} onmousedown={inc} onmouseup={mouseUp}>+</Button>
 </wrapper>
 
 <style lang="scss">
-  $text: var(--text);
-
   wrapper {
     display: flex;
     justify-content: space-around;
     align-items: center;
     width: 300px;
-  }
-
-  input {
-    outline: none;
-    text-align: center;
-    border: 0;
-    width: 100px;
-    font: 20px $fira;
-    color: $text;
-    border-bottom: 1px solid transparent;
-    background: transparent;
-    transition: 0.1s ease;
-    border-radius: 4px;
-    &:hover {
-      background: white;
-      color: black;
-    }
-    &:focus {
-      color: black;
-      background: white;
-    }
-
-    &::-webkit-outer-spin-button,
-    &::-webkit-inner-spin-button {
-      -webkit-appearance: none;
-      margin: 0;
-    }
   }
 </style>
---
+++ src/input.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let value = "";
+  export let color = {};
+</script>
+
+<input bind:value type="number" style="--text:{color.text}" />
+
+<style lang="scss">
+  $text: var(--text);
+
+  input {
+    outline: none;
+    text-align: center;
+    border: 0;
+    width: 100px;
+    font: 20px $fira;
+    color: $text;
+    border-bottom: 1px solid transparent;
+    background: transparent;
+    transition: 0.1s ease;
+    border-radius: 4px;
+    &:hover {
+      background: white;
+      color: black;
+    }
+    &:focus {
+      color: black;
+      background: white;
+    }
+
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+  }
+</style>
